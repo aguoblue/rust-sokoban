9e6dd6d02ab43291bad8b09cbab070573414cb25
aguoblue <aguogggg@outlook.com>
2025-01-09 14:42:36 +0800

    glam，渲染3个实体

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,73 @@
-use ggez::{conf, event, Context, GameResult};
+use ggez::{
+    conf, event,
+    graphics::{self, DrawParam, Image},
+    Context, GameResult,
+};
+use glam::Vec2;
 use hecs::{Entity, World};
 
 use std::path;
+
+const TILE_WIDTH: f32 = 32.0;
+
+// ANCHOR: init
+// Initialize the level
+pub fn initialize_level(world: &mut World) {
+    create_player(
+        world,
+        Position {
+            x: 0,
+            y: 0,
+            z: 0, // we will get the z from the factory functions
+        },
+    );
+    create_wall(
+        world,
+        Position {
+            x: 1,
+            y: 0,
+            z: 0, // we will get the z from the factory functions
+        },
+    );
+    create_box(
+        world,
+        Position {
+            x: 2,
+            y: 0,
+            z: 0, // we will get the z from the factory functions
+        },
+    );
+}
+// ANCHOR_END: init
+
+pub fn run_rendering(world: &World, context: &mut Context) {
+    // Clearing the screen (this gives us the background colour)
+    let mut canvas =
+    graphics::Canvas::from_frame(context, graphics::Color::from([0.95, 0.95, 0.95, 1.0]));
+
+    // Get all the renderables with their positions and sort by the position z
+    // This will allow us to have entities layered visually.
+    let mut query = world.query::<(&Position, &Renderable)>();
+    let mut rendering_data: Vec<(Entity, (&Position, &Renderable))> = query.into_iter().collect();
+    rendering_data.sort_by_key(|&k| k.1 .0.z);
+
+    // Iterate through all pairs of positions & renderables, load the image
+    // and draw it at the specified position.
+    for (_, (position, renderable)) in rendering_data.iter() {
+        // Load the image
+        let image = Image::from_path(context, renderable.path.clone()).unwrap();
+        let x = position.x as f32 * TILE_WIDTH;
+        let y = position.y as f32 * TILE_WIDTH;
+
+        // draw
+        let draw_params = DrawParam::new().dest(Vec2::new(x, y));
+        canvas.draw(&image, draw_params);
+    }
+
+    // Finally, present the canvas, this will actually display everything
+    // on the screen.
+    canvas.finish(context).expect("expected to present");
+}
 
 
 
@@ -27,8 +93,12 @@ impl event::EventHandler<ggez::GameError> for Game {
         Ok(())
     }
 
-    fn draw(&mut self, _context: &mut Context) -> GameResult {
+    fn draw(&mut self, context: &mut Context) -> GameResult {
         // TODO: update draw here
+        // Render game entities
+        {
+            run_rendering(&self.world, context);
+        }
         Ok(())
     }
 }
@@ -116,10 +186,10 @@ pub fn create_player(world: &mut World, position: Position) -> Entity {
 // ANCHOR_END: entities
 
 
-
 // ANCHOR: main
 pub fn main() -> GameResult {
-    let world = World::new();
+    let mut  world = World::new();
+    initialize_level(&mut world);
 
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
